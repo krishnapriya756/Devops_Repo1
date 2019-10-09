fcd30ed8da58874b4a7b2c0061bed4bba4ff5596
krishnapriya756 <56140619+krishnapriya756@users.noreply.github.com>
2019-10-09 18:26:47 -0400

    test232

--- JavaSampleProject1/src/samplepackage1/pkg/FirstClass.java
+++ JavaSampleProject1/src/samplepackage1/pkg/FirstClass.java
@@ -5,11 +5,11 @@ public class FirstClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("My First Program");
-		int a, b, c;
+		int a, b, c1;
 		a = 5;
 		b = 30;
-		c = a + b;
-		System.out.println("Updated one : c" + c);
+		c1 = a + b;
+		System.out.println("Updated one : c" + c1);
 	
 
 	}
